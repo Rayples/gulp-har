14ff7f6fd5f11fca37ca487c4f8c308d16a41e09
Rayple <rayple@126.com>
2024-08-31 09:44:24 +0800

    tag: merge from w

--- test/gulpfile-ems.js
+++ test/gulpfile-ems.js
@@ -1,25 +1,32 @@
 import gulp_har from 'gulp-har';
 import gulp from 'gulp';
 
-gulp.task('parse', (done) => {
-    gulp.src("./har/*.har")
-        .pipe(gulp_har({
-            beautify: true,
-            override: ["!.json"],
-            request: {
-                filter: {
-                    url: (request, url) => /^(?!(.*?blob\/master)).*/.test(url),
-                }
-            },
-            response: {
-                filter: {
-                    "status": /^[^45]\d+/,
-                    "content.text": (request, text) => !!text && !!text.trim()
-                }
+gulp.task("parse", (done) => {
+    gulp
+      .src("./har/*.har")
+      .pipe(
+        gulp_har({
+          beautify: true,
+          override: false,
+          request: {
+            filter: {
+              url: (request, url) => {
+                return !/(blob|raw|tree)\/master/.test(url);
+              }
             }
-        }))
-        .pipe(gulp.dest("./dest"));
+          },
+          response: {
+            filter: {
+              status: /^[^45]\d+/,
+              "content.text": (request, text) => {
+                return text === undefined ? true : !!text.trim();
+              }
+            }
+          }
+        })
+      )
+      .pipe(gulp.dest("./dest"));
     done();
-});
+  });
 
 gulp.task("default", gulp.series("parse"));
